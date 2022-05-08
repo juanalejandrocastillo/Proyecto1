00cad772d31b086cb3ede1a2175ebf450e981db3
juanalejandrocastillo <juanalejandrocastillo1989@gmail.com>
2022-05-08 19:43:56 -0300

    links carousel Added

--- src/componentes/Slides.js
+++ src/componentes/Slides.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import '../hojas-de-estilo/Slider.css';
 
-const slidesInfo = [
-    {
+const slidesInfo =
+    [
+        { 
         src: 'https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_960_720.jpg',
-        alt: 'Proyect 1',
-        desc: 'Proyect 1',
+        alt: 'Calculadora React',
+        desc: <a className='links'href='https://juanalejandrocastillo.github.io/calculadoraReact/' target='_blank' rel='noreferrer'>
+                Calculadora React
+                </a>,
     },
-    {
+        {
         src: 'https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg',
-        alt: 'Proyect 2',
-        desc: 'Proyect 2',
+        alt: 'Contador de Clics',
+        desc: <a className='links'href='https://juanalejandrocastillo.github.io/contador-click/' target='_blank' rel='noreferrer'>
+                Contador de Clics
+                </a>,
     },
     {
-        src: 'https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg',
+        src: 'https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg' ,
         alt: 'Proyect 3',
-        desc: 'Proyect 3',
+        desc: <a className='links'href='https://juanalejandrocastillo.github.io/aplicacion-tareas/' target='_blank' rel='noreferrer'>
+            Aplicacion de Tareas
+            </a>,
     },
 ]
 
 const slides = slidesInfo.map(slide => (
     <div className='slide-container'>
-        <img src={slide.src} alt={slide.alt} />
+        
+        <img src={slide.src}   alt={slide.alt} />
         <div className='slide-desc'>
             <span>{slide.desc}</span>
         </div>
